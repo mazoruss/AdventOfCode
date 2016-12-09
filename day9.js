8459a8b548b6cb64c466bbeabc2dd0cc933cffeb
Jace <chesszhu@gmail.com>
2016-12-08 21:55:07 -0800

    day 9 complete, rank 188

--- day9.js
+++ day9.js
@@ -2,6 +2,69 @@ var fs = require('fs');
 
 var input = fs.readFileSync('./day9.txt', "utf8");
 
-console.log(input)
+// var input = 'X(8x2)(3x3)ABCY';
 
-input = input.split('\n');
+// var result = '';
+
+var decomp = (string) => {
+	var n = 0;
+	for (var i = 0; i < string.length; i++) {
+		if (string[i] === '(') {
+			var comp = '';
+			i++;
+			while (string[i] !== ')') {
+				comp += string[i];
+				i++;
+			}
+			i++;
+			comp = comp.split('x');
+			var leng = Number(comp[0]);
+			var times = Number(comp[1]);
+			n += decomp(string.slice(i, i + leng)) * times;
+			i += leng - 1;
+		} else {
+			n++;
+		}
+	}
+	return n;
+}
+
+console.log(decomp(input));
+
+// var leng = 0;
+// var times = 0;
+
+// var decomp = (times, leng, string) {
+// 	if ()
+// 	return string;
+// }
+
+// for (var i = 0; i < input.length; i++) {
+// 	var cur = input[i];
+// 	var comp = '';
+// 	if (cur === '(') {
+// 		i++;
+// 		while (input[i] !== ')') {
+// 			comp += input[i];
+// 			i++;
+// 		}
+// 		i++;
+// 		comp = comp.split('x');
+// 		leng = Number(comp[0]);
+// 		times = Number(comp[1]);
+
+// 		var string = '';
+// 		var index = i;
+// 		for (var j = 0; j < times; j++) {
+// 			for (var k = 0; k < leng; k++) {
+// 				string += input[i+k]
+// 			}
+// 		}
+// 		result += string;
+// 		i += k - 1;
+// 	} else {
+// 		result += cur;
+// 	}
+// }	
+
+// console.log(result.length);
